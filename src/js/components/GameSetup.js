b4915755b3845a5b8d0724a7b262954704ff20cc
Ryan Thomas <ryanthomas30@gmail.com>
2018-05-21 10:50:18 -0400

    Add dropdown menu and sidebar

--- src/js/components/GameSetup.js
+++ src/js/components/GameSetup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Label, Icon } from 'semantic-ui-react';
+import { Segment, Label, Icon, Dropdown, Header, Divider, Button, Sidebar, Menu } from 'semantic-ui-react';
 
 import { players1 } from './mockdata';
 
@@ -17,6 +17,7 @@ const styles = {
 
 class GameSetup extends Component {
 	render() {
+		const icon = <Icon name='ellipsis vertical' size='large' />;
 		const renderPlayers = () => {
 			return players1.map((player, i) => {
 				console.log('player: ', player);
@@ -28,7 +29,14 @@ class GameSetup extends Component {
 								<Label style={{ marginRight: '12px' }} >
 									{player.role || 'Unassigned'}
 								</Label>
-								<Icon name='ellipsis vertical' size='large' />
+								<Dropdown icon={icon} >
+									<Dropdown.Menu>
+										<Dropdown.Item>Assign Role</Dropdown.Item>
+										<Divider />
+										<Dropdown.Item>Move Up</Dropdown.Item>
+										<Dropdown.Item>Move Down</Dropdown.Item>
+									</Dropdown.Menu>
+								</Dropdown>
 							</div>
 						</div>
 					</Segment>
@@ -36,9 +44,42 @@ class GameSetup extends Component {
 			});
 		};
 		return (
-			<div style={styles.mainContent} >
-				{renderPlayers()}
-			</div>
+			<Sidebar.Pushable style={{ height: window.innerHeight }} >
+				<div style={styles.mainContent} >
+					<Sidebar
+						as={Menu}
+						animation='push'
+						width='thin'
+						direction='bottom'
+						visible={true}
+						icon='labeled'
+						vertical >
+						<Menu.Item name='home'>
+							<Icon name='home' />
+							Home
+						</Menu.Item>
+						<Menu.Item name='gamepad'>
+							<Icon name='gamepad' />
+							Games
+						</Menu.Item>
+						<Menu.Item name='camera'>
+							<Icon name='camera' />
+							Channels
+						</Menu.Item>
+					</Sidebar>
+					<Sidebar.Pusher>
+						<div>
+							<Header as='h1' >
+							Select Roles
+							</Header>
+							<Button primary >Confirm</Button>
+							<Button>Randomize</Button>
+							<Divider />
+						</div>
+						{renderPlayers()}
+					</Sidebar.Pusher>
+				</div>
+			</Sidebar.Pushable>
 		);
 	}
 }
