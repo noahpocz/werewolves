cbecebfdcba410152033fff47de63bb5d9437017
Ryan Thomas <ryanthomas30@gmail.com>
2018-06-09 20:08:19 -0400

    Implement basic role tracking

--- src/js/components/RoleList.js
+++ src/js/components/RoleList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Header, Divider, Button, Card, Image } from 'semantic-ui-react';
+import { Header, Divider, Button, Card, Image, Label, Icon } from 'semantic-ui-react';
 
 import { roles } from './mockdata';
 import * as actions from '../actions/players';
@@ -35,21 +35,50 @@ class RoleList extends Component {
 		const { index } = this.props.match.params;
 		players = players || [];
 		const currentRole = players[index].role ? players[index].role.name : 'Unassigned';
+		roles.forEach(role => {
+			role.count = 0;
+			players.forEach(player => {
+				if (player.role) {
+					if (role.name === player.role.name) {
+						role.count++;
+					}
+				}
+			});
+		});
 		const renderRoles = () => {
 			return roles.map((role, i) => {
 				let cardColor = '';
+				let labelColor = '';
+				switch (role.team) {
+					case 'Werewolves':
+						labelColor = 'red';
+						break;
+					case 'Villagers':
+						labelColor = 'brown';
+						break;
+					case 'Other':
+						labelColor = 'purple';
+						break;
+					default:
+						labelColor = '';
+						break;
+				}
+				const teamLabel =
+					<Label style={{ marginRight: '12px' }} color={labelColor} >
+						{role.team}
+					</Label>;
 				if (players[index].role) {
 					cardColor = role.name === players[index].role.name ? 'blue' : '';
 				}
 				return (
 					<Card key={i} color={cardColor} >
-						<Image src={role.image} />
+						<Image src={role.image} label={<Label corner='right' >{role.count}/1</Label>} />
 						<Card.Content>
 							<Card.Header>
 								{role.name}
 							</Card.Header>
 							<Card.Meta>
-								{role.team}
+								{teamLabel}
 							</Card.Meta>
 							<Card.Description>
 								{role.description}
--- src/js/components/mockdata.js
+++ src/js/components/mockdata.js
@@ -68,61 +68,61 @@ export const roles = [
 	},
 	{
 		name: 'Witch',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'The witch has two spooky potions.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Elder',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'The elder has fallen and can\'t get up.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Fox',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'The fox can find out if you\'re spooky.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Little Girl',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'She looks at werewolves.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Hunter',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'His gun goes pew pew when he dies.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Cupid',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'Makes people fall in love.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Two Sisters',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'There are two of them.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Three Brothers',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'There are three of them.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Scapegoat',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'Point to him when things go bad.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 	{
 		name: 'Village Idiot',
-		team: 'Villager',
+		team: 'Villagers',
 		description: 'Does dumb things.',
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
@@ -133,3 +133,7 @@ export const roles = [
 		image: 'https://cdn.myminifactory.com/assets/object-assets/5971a8bd270db/images/720X720-beto-thingiverse.jpg'
 	},
 ];
+
+export const metadata = {
+
+};
